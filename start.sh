9cbe43744b006da4f1899f56eea5ce91cb85e0d9
xiaosongsong <1120344670@qq.com>
2020-04-12 16:18:21 +0800

    ioevent

--- start.sh
+++ start.sh
@@ -3,3 +3,8 @@
 hugo --baseUrl="/"
 tar -cvzf blog.tar.gz public/
 scp -r -i ~/.ssh/authorized_keys -P 102  ./public/ root@47.100.94.219:/opt/my-blog
+rm -rf blog.tar.gz
+
+git add .
+git commit -m $1
+git push
